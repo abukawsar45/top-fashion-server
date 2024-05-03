d7d92c8e166c2c7ff8e8279fb56cb1f92fac6e4f
abukawsar45 <kawsar1146236@gmail.com>
2024-05-03 09:55:51 +0600

    connected with mongodb

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require("express");
+const cors =require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.DB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+// topFashionDb
+// allProducts
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get("/", (req, res)=>{
+  res.send(`Top fashion server is runnig on port ${port}`)
+})
+
+app.listen(port, ()=>{
+  console.log(`Top fashion server is runnig on port ${port}`)
+})
